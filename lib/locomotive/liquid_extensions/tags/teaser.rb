2ee127480a775f44522067693ecc9cc307b1655f
ziedbargaoui <bargaouizied@gmail.com>
2015-07-08 12:51:33 +0200

    Teasers

--- lib/locomotive/liquid_extensions/tags/teaser.rb
+++ lib/locomotive/liquid_extensions/tags/teaser.rb
@@ -10,9 +10,18 @@ module Locomotive
           context_test = Hash.new
           context_test[:site] = context.registers[:site]
 
+
           content = ""
-          context.registers[:page].snippet_dependencies.each do |i|
-            content = content + "{% include '#{i}' %}"
+          context.registers[:page].teasers_dependencies.each do |i|
+            #content = content + "{% include '#{i}' %}"
+            teasers_content_type = context.registers[:site].content_types.where(slug: 'teasers').first
+
+            teasers_content_entries = context.registers[:site].content_entries.where(content_type_id: teasers_content_type.id, _slug: i)
+
+            teasers_content_entries.each do |teaser|
+              content << '<div class="home-teaser">
+              <div class="home-teaser-title">'+teaser.teaser_name+'</div><hr><div class="home-teaser-content">'+teaser.teaser_body+'</div></div>'
+            end
           end
 
           @template = ::Liquid::Template.parse(content,context.merge(context_test))
