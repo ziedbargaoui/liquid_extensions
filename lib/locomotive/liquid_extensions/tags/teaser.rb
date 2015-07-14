38e9868ab911bdc23ab542a48f25de9ed0badc20
ziedbargaoui <bargaouizied@gmail.com>
2015-07-14 15:32:24 +0200

    Teasers mini/home

--- lib/locomotive/liquid_extensions/tags/teaser.rb
+++ lib/locomotive/liquid_extensions/tags/teaser.rb
@@ -4,12 +4,48 @@ module Locomotive
 
       class Teaser < Solid::Tag
 
+        def display(*options, &block)
 
-        def render(context)
+          model_name, attributes = self.extract_model_name_and_attributes(options)
+          @new_att = attributes
+          self.render_teasers(@current_context)
+        end
+
+        def extract_model_name_and_attributes(options)
+
+          raise ::Liquid::Error.new('[EntriesOverview] wrong number of parameters (1 is required)') if options.size < 1
+
+          [options.first.to_s, options.last].tap do |name, attributes|
+            if attributes.is_a?(Hash)
+              attributes.each do |k, v|
+                # deal with the model directly instead of the liquid drop
+                _source = v.instance_variable_get(:@_source)
+
+                attributes[k] = _source if _source
+              end
+
+              # the content entry should not be attached to another site or content type
+              attributes.delete_if { |k, _| %w(site site_id content_type content_type_id).include?(k) }
+            else
+              #raise ::Liquid::Error.new('[form_builder] wrong attributes')
+              attributes
+            end
+          end
+        end
+
+
+        def render_teasers(context)
 
           context_test = Hash.new
           context_test[:site] = context.registers[:site]
 
+          if @new_att[:mode]=='mini'
+            div_class_prefix = 'mini'
+            under_title = '<img class="teaser-pfeil" src={{ "orange-pfeil.png" | theme_image_url }}>'
+          else
+            div_class_prefix = 'home'
+            under_title = '<hr>'
+          end
 
           content = ""
           context.registers[:page].teasers_dependencies.each do |i|
@@ -19,8 +55,11 @@ module Locomotive
             teasers_content_entries = context.registers[:site].content_entries.where(content_type_id: teasers_content_type.id, _slug: i)
 
             teasers_content_entries.each do |teaser|
-              content << '<div class="home-teaser">
-              <div class="home-teaser-title">'+teaser.teaser_name+'</div><hr><div class="home-teaser-content">'+teaser.teaser_body+'</div></div>'
+              content << '<div class="'+div_class_prefix+'-teaser">
+              <div class="'+div_class_prefix+'-teaser-title">'+teaser.teaser_name+'</div>'
+              content << under_title
+              content << '<div class="'+div_class_prefix+'-teaser-content">'+teaser.teaser_body+'</div>
+              </div>'
             end
           end
 
